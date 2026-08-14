79eb19e4e4d4f8926b1e5ade403133ae5cb56ccc
Takaaki Nishioka <nishiokya@gmail.com>
2026-08-14 22:30:16 +0900

    Add AdSense site verification (#221)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,6 +70,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <head>
+        <meta name="google-adsense-account" content="ca-pub-6885302916426075" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
